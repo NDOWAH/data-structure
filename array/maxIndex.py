a4ed92e51a3fb4a35b8d8d3ec33510bc2fdafd52
Marcel grossmann <marcelgrossmann80@gmail.com>
2023-12-29 20:02:56 +0100

    add doctest

--- array/maxIndex.py
+++ array/maxIndex.py
@@ -1,9 +1,17 @@
-def maxIndex(arr) -> str:
+import doctest
+
+def maxIndex(arr) -> int:
     """
     This functions takes in an array with its size and return the max of j-i such that
     arr[i] <= arr[j]
     :param arr: integer array
     :return: int
+    >>> maxIndex([9, 2, 3, 4, 5, 6, 7, 8, 18])
+    8
+    >>> maxIndex([34, 8, 10, 3, 2, 80, 30, 33, 1])
+    6
+    >>> maxIndex([1, 2, 3, 4, 5, 6])
+    5
     """
     # To do: modify code to track the i and j
     # positions for which max difference occurred
@@ -13,9 +21,7 @@ def maxIndex(arr) -> str:
         for j in range(i, len(arr)):
             if arr[i] - arr[j] > 0 or i == j:
                 continue
-            print(f'{i}<-->{j}')
             index_differences.append(j - i)
     return max(index_differences)
 
 
-print(maxIndex([2, 9, 8, 1, 20]))
