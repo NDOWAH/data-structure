4ffeb0072393fff385e943a7404f0853d6f27abc
Marcel grossmann <marcelgrossmann80@gmail.com>
2023-12-30 20:55:06 +0100

    add array reverse function

---
+++ array/reverseAarray.py
@@ -0,0 +1,22 @@
+
+def reverseArray(arr, arr_size) -> type:
+    """
+    This functions takes an array and return its reverse.
+    :param arr: the array we want to reverse
+    :param arr_size: size of the array we want to reverse
+    :return: type of arr
+    """
+    reverse_array = []
+    if arr_size == 1 or arr_size == 0:
+        return arr
+    else:
+        while arr_size >= 0:
+            reverse_array.append(arr[arr_size-1])
+            arr_size -= 1
+    return reverse_array
+
+
+print(reverseArray([1, 2, 6, 8, 9], 5))
+
+
+
