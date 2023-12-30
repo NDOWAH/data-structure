a02e577d357bb7671dc3e964bf172637f1cedf20
Marcel grossmann <marcelgrossmann80@gmail.com>
2023-12-30 21:03:48 +0100

    correct reverse function

--- array/reverseAarray.py
+++ array/reverseAarray.py
@@ -1,5 +1,5 @@
 
-def reverseArray(arr, arr_size) -> type:
+def reverseArray(arr, arr_size):
     """
     This functions takes an array and return its reverse.
     :param arr: the array we want to reverse
